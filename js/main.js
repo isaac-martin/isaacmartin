e4a523ebe77d5235d058c528f99e1c18f8bbfe84
isaac-martin <isaac@weareplayground.co>
2017-02-24 16:26:23 +1100

    New Files

--- js/main.js
+++ js/main.js
@@ -1,70 +1,28 @@
-
-
-//	HISTORICIZE AND AJAXIFY OUR SITE
-$(document).ready(function() {
-	var siteUrl = 'http://'+(document.location.hostname||document.location.host);
-
-	//	Catch all internally-focused links and push a new state.
-	//	Note: External links will not be affected by this behavior.
-	$(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
-		e.preventDefault();
-		History.pushState({}, "", this.pathname);
-		$("html, body").animate({ scrollTop: 0 }, "slow");
-	});
-
-	History.Adapter.bind(window, 'statechange', function(){
-		var State = History.getState();
-		$.get(State.url, function(data){	// Use AJAX to get the new content.
-			document.title = data.match(/<title>(.*?)<\/title>/)[1];
-			$('.ajax').html($(data).find('.ajax').parent());
-
-			//_gaq.push(['_trackPageview', State.url]);
-			// This updates Google Analytics with a visit to the new page.
-		});
-	});
-		initMap();
-});
-// Custom JS
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 200) {
-        $("body").addClass("scrolled");
-    } else {
-        $("body").removeClass("scrolled");
-    }
-});
-
-
-$(document).ajaxComplete(function(){
-	initMap();
-});
-
-// var feed = new Instafeed({
-// 				target: 'instagram'
-//         get: 'user',
-//         userId: 'sportsclinicmelb',
-//         clientId: 'f75587d885b54a9ba9453d07f36c9055'
-//     });
-// feed.run();
-
-
-$.fn.resizer = function(){
-	var wide = $('.service-bg').css('width');
-	var calculate = parseInt(wide, 10)* 0.6;
-	$('.service-bg').css('height', calculate);
-};
-
-$(document).ready(function(){
-	 	$().resizer();
- $("input,textarea").focus(function(){
-   $(this).parent().addClass("focused");
-  }).blur(function(){
-       $(this).parent().removeClass("focused");
-  })
-});
-
-
-$(window).on('resize',function() {
- 	$().resizer();
+$(function(){
+  'use strict';
+  var $page = $('#main'),
+      options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 2,
+        onStart: {
+          duration: 1000, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $container.addClass('is-exiting');
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onReady: {
+          duration: 0,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $container.removeClass('is-exiting');
+            // Inject the new content
+            $container.html($newContent);
+          }
+        }
+      },
+      smoothState = $page.smoothState(options).data('smoothState');
 });
