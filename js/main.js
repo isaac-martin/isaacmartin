fc019585ef373424431fc8096334ffd2712dc31a
isaac-martin <isaac@weareplayground.co>
2017-03-14 12:26:05 +1100

    Added Pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,52 @@
 $(function(){
-  'use strict';
+'use strict';
+
+
+$.fn.forms = function() {
+
+var $contactForm = $('#contact-form');
+
+ $contactForm.submit(function(e) {
+ 	e.preventDefault();
+ 	var $submit = $('input:submit', $contactForm);
+ 	var defaultSubmitText = $submit.val();
+
+ 	$.ajax({
+ 		url: '//formspree.io/contact@isaacmartin.co',
+ 		method: 'POST',
+ 		data: $(this).serialize(),
+ 		dataType: 'json',
+ 		beforeSend: function() {
+ 			//$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+ 			$submit.attr('disabled', true).val('Sending message…');
+ 		},
+ 		success: function(data) {
+ 			//$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+ 			$submit.val('Message sent!');
+ 			setTimeout(function() {
+ 				//$('.alert--success').remove();
+ 				$submit.attr('disabled', false).val(defaultSubmitText);
+ 			}, 5000);
+ 		},
+ 		error: function(err) {
+ 			//$contactForm.find('.alert--loading').hide();
+ 			//$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+ 			$submit.val('Ops, there was an error.');
+ 			setTimeout(function() {
+ 				//$('.alert--error').remove();
+ 				$submit.attr('disabled', false).val(defaultSubmitText);
+ 			}, 5000);
+ 		}
+ 	});
+ });
+
+}
+
+
+  $( document ).ready(function() {
+      $().forms();
+  });
+
   var $page = $('#main'),
       options = {
         debug: true,
@@ -23,6 +70,9 @@ $(function(){
             // Inject the new content
             $container.html($newContent);
           }
+        },
+        onAfter: function( $container ) {
+            $().forms();
         }
       },
       smoothState = $page.smoothState(options).data('smoothState');
